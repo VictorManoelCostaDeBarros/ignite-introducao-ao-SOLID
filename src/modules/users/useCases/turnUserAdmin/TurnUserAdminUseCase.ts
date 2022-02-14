8a61391d4ad39119cf83b5df91669ee583d702b1
Victor Manoel <victor.manoel8@hotmail.com>
2022-02-14 20:12:18 -0300

    challenge completed

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -6,10 +6,16 @@ interface IRequest {
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userById = this.usersRepository.findById(user_id)
+    if (!userById) {
+      throw new Error('User not exists')
+    }
+    const user = this.usersRepository.turnAdmin(userById)
+
+    return user
   }
 }
 
