e85bfadf977762e830dbbfd1f4f476d3d683c16c
Victor Manoel <victor.manoel8@hotmail.com>
2022-02-17 20:14:39 -0300

    documentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
+const router = express.Router();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.use("/users", usersRoutes);
 
--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,10 +3,19 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor(private createUserUseCase: CreateUserUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { name, email } = request.body
+
+      this.createUserUseCase.execute({ email, name })
+
+      return response.status(201).send()
+    } catch (error) {
+      return response.status(400).json({ error: error })
+    }
+
   }
 }
 
